ea3e6a979d5890513750a951174ced103d8e247d
gmp26 <gmp26@cam.ac.uk>
2019-12-19 15:38:55 +0000

    created setup.R

--- R/setup.R
+++ R/setup.R
@@ -1,8 +1,10 @@
 library(devtools)
 library(opencpu)
-install_github("gmp26/cats")
+install_github("gmp26/v22")
 library(cats)
 opencpu::ocpu_start_server()
 
 
 # curl -d'love=FALSE' http://localhost:5656/ocpu/library/cats/R/cat_function
+
+# Also, see `server.Rmd` in vignettes
